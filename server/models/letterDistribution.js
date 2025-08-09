ae1a3f2333cfe7f5ec91f5068420e290922193e8
Reailiteen <ad2313830@qu.edu.qa>
2025-08-09 20:37:52 +0300

    Remove server model files for board, letter, letterDistribution, move, player, and room to streamline the project structure.

--- server/models/letterDistribution.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const letterSchema = require("./letter");
-
-const letterDistributionSchema = new mongoose.Schema({
-  letters: [letterSchema]
-});
-
-module.exports = letterDistributionSchema;
